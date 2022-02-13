d1809631cc7d967c24140c9f7aa03e7bd9677f2a
github daisukenoguchi <daisukenoguemon@gmail.com>
2022-02-13 19:39:03 +0900

    update workd

--- pages/works.vue
+++ pages/works.vue
@@ -57,7 +57,7 @@ export default {
           title: 'NUXTEC',
           link: 'https://d21iuqrezm3div.cloudfront.net',
           description:
-            'Nuxtで作った初めてのサイト。ServerlessでECサイトを作ってみたかった。',
+            'ServerlessでECサイトを作ってみたかった。stripeは本番使用じゃないので平気\n sample email: testuser@gmail.com\n sample pass: testuser\n sample card: 4242424242424242/ ',
           img: 'https://i.gyazo.com/6de28f5cfbf3973f4aebf898a6706b05.png',
           techs: ['Nuxt', 'Express', 'Serverless', 'Aws'],
           article: '',
@@ -66,7 +66,7 @@ export default {
           title: 'KUYOU',
           link: 'https://haraichi-kuyou.com/',
           description:
-            '読まれなかったコーナーメールを供養するために作ったサービス。Apollo-composable composition-apiの挙動にかなり悩まされた',
+            '読まれなかったコーナーメールを供養するために作ったサービス。のはずが、サイト自体が供養されてしまうという皮肉',
           img: 'https://i.gyazo.com/2e40f44f5156ab0e3f97fabd4937828d.jpg',
           techs: ['Vue2', 'Graphql', 'Express', 'apollo-client'],
           article: '',
@@ -75,7 +75,7 @@ export default {
           title: 'これ',
           link: 'https://gue-techblog.com/',
           // link: 'https://gue-tech-blog.ga/',
-          description: '自作ブログ。Contentfulがマジ便利',
+          description: 'JAMstack体験型ブログ',
           img: 'https://i.gyazo.com/d36a17a8d779e01feeba99bd73e30a14.png',
           techs: ['Nuxt', 'Contentful'],
           article: '',
@@ -83,7 +83,7 @@ export default {
         {
           title: 'nbaスレ翻訳',
           link: 'https://www.nba-honyaku.net/posts/sr5jzi',
-          description: 'redditのnba関連スレッドを自動で日本語翻訳して表示するサイト',
+          description: 'redditのを自動で日本語翻訳して表示するサイト',
           img: 'https://i.gyazo.com/510aa2967aae77155db7ba759eb3ed5a.png',
           techs: ['Vue', 'Golang', 'Lambda', 'dynamodb'],
           article: '',
@@ -92,7 +92,7 @@ export default {
           title: 'Gnavi',
           link: 'https://gue-tech-blog.ga/',
           description:
-            'Airbnbとぐるなびを合体して見たかった。RDS, ECSインスタンスがお高いので基本動かいてないです()',
+            'RDS, ECSインスタンスがお高いので基本動かいてないです()',
           img: 'https://i.gyazo.com/3d5bef0f5322b520deabb562dfc534c5.jpg',
           techs: ['rails', 'ECS', 'CircleCI'],
           article: '',
